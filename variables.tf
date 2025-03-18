bd4537951c8ed3c71c2d89ac76211d76e264d77e
suman.dhakal <suman.dhakal@agribora.com>
2025-03-18 18:28:50 +0100

    added variables for eks

--- variables.tf
+++ variables.tf
@@ -35,3 +35,36 @@ variable "single_nat_gateway" {
 variable "enable_dns_hostnames" {
   type = bool
 }
+
+# Kubernetes
+variable "cluster_version" {
+  type = string
+}
+
+variable "min_instance_count" {
+  type        = number
+  description = "EKS min node count"
+}
+
+variable "max_instance_count" {
+  type        = number
+  description = "EKS max node count"
+}
+
+variable "desired_instance_count" {
+  type        = number
+  description = "EKS desired node count"
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "node_name" {
+  type = string
+}
+
+variable "eks_managed_node_group_defaults_ami_type" {
+  type = string
+}
+
